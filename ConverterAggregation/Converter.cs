ca6faed2e4acc04b8bb987196b5246e9213a3ea1
marchanyanehu <dadada.marchan@gmail.com>
2023-06-30 22:24:35 +0300

    done

--- ConverterAggregation/Converter.cs
+++ ConverterAggregation/Converter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace ConverterDictionaryAggregation
 {
@@ -7,6 +9,8 @@ namespace ConverterDictionaryAggregation
     /// </summary>
     public class Converter
     {
+        private readonly CharsDictionary charsDictionary;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Converter"/> class.
         /// </summary>
@@ -15,7 +19,12 @@ namespace ConverterDictionaryAggregation
         /// <exception cref="System.ArgumentException">Thrown when charsDictionary.Dictionary is empty.</exception>
         public Converter(CharsDictionary? charsDictionary)
         {
-            throw new NotImplementedException();
+            this.charsDictionary = charsDictionary ?? throw new ArgumentNullException(nameof(charsDictionary));
+
+            if (charsDictionary.Dictionary == null || charsDictionary.Dictionary.Count == 0)
+            {
+                throw new ArgumentException("Dictionary cannot be empty.", nameof(charsDictionary));
+            }
         }
 
         /// <summary>
@@ -25,7 +34,73 @@ namespace ConverterDictionaryAggregation
         /// <returns>A number string representation.</returns>
         public string Convert(double number)
         {
-            throw new NotImplementedException();
+            if (double.IsNaN(number))
+            {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                return this.charsDictionary.Dictionary[Character.NaN];
+            }
+
+            if (double.IsPositiveInfinity(number))
+            {
+                return this.charsDictionary.Dictionary[Character.PositiveInfinity];
+            }
+
+            if (double.IsNegativeInfinity(number))
+            {
+                return this.charsDictionary.Dictionary[Character.NegativeInfinity];
+            }
+
+            if (number == double.Epsilon)
+            {
+                return this.charsDictionary.Dictionary[Character.Epsilon];
+            }
+
+            CultureInfo culture = this.charsDictionary.CultureName != null ? new CultureInfo(this.charsDictionary.CultureName) : CultureInfo.InvariantCulture;
+            var numberString = number.ToString(culture);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var ch in numberString)
+            {
+                if (this.charsDictionary.Dictionary.TryGetValue(GetCharacter(ch), out var word))
+                {
+                    if (stringBuilder.Length > 0)
+                    {
+                        stringBuilder.Append(' ');
+                    }
+
+                    stringBuilder.Append(word);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Character '{ch}' is not supported by the provided CharsDictionary.");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private static Character GetCharacter(char ch)
+        {
+            return ch switch
+            {
+                '0' => Character.Zero,
+                '1' => Character.One,
+                '2' => Character.Two,
+                '3' => Character.Three,
+                '4' => Character.Four,
+                '5' => Character.Five,
+                '6' => Character.Six,
+                '7' => Character.Seven,
+                '8' => Character.Eight,
+                '9' => Character.Nine,
+                '+' => Character.Plus,
+                '-' => Character.Minus,
+                '.' => Character.Point,
+                ',' => Character.Comma,
+                'E' => Character.Exponent,
+                'e' => Character.Exponent,
+                _ => throw new ArgumentException($"Unsupported character '{ch}'.")
+            };
         }
     }
 }
--- ConverterAggregation/Сharacter.cs
+++ ConverterAggregation/Сharacter.cs
@@ -5,24 +5,99 @@
     /// </summary>
     public enum Character : byte
     {
+        /// <summary>
+        /// Represents the digit 0.
+        /// </summary>
         Zero,
+
+        /// <summary>
+        /// Represents the digit 1.
+        /// </summary>
         One,
+
+        /// <summary>
+        /// Represents the digit 2.
+        /// </summary>
         Two,
+
+        /// <summary>
+        /// Represents the digit 3.
+        /// </summary>
         Three,
+
+        /// <summary>
+        /// Represents the digit 4.
+        /// </summary>
         Four,
+
+        /// <summary>
+        /// Represents the digit 5.
+        /// </summary>
         Five,
+
+        /// <summary>
+        /// Represents the digit 6.
+        /// </summary>
         Six,
+
+        /// <summary>
+        /// Represents the digit 7.
+        /// </summary>
         Seven,
+
+        /// <summary>
+        /// Represents the digit 8.
+        /// </summary>
         Eight,
+
+        /// <summary>
+        /// Represents the digit 9.
+        /// </summary>
         Nine,
+
+        /// <summary>
+        /// Represents the plus sign (+).
+        /// </summary>
         Plus,
+
+        /// <summary>
+        /// Represents the minus sign (-).
+        /// </summary>
         Minus,
+
+        /// <summary>
+        /// Represents the decimal point (.).
+        /// </summary>
         Point,
+
+        /// <summary>
+        /// Represents the decimal comma (,).
+        /// </summary>
         Comma,
+
+        /// <summary>
+        /// Represents the exponent symbol (E or e).
+        /// </summary>
         Exponent,
+
+        /// <summary>
+        /// Represents the Not-a-Number (NaN) value.
+        /// </summary>
         NaN,
+
+        /// <summary>
+        /// Represents the positive infinity value.
+        /// </summary>
         PositiveInfinity,
+
+        /// <summary>
+        /// Represents the negative infinity value.
+        /// </summary>
         NegativeInfinity,
+
+        /// <summary>
+        /// Represents the smallest positive double value greater than zero.
+        /// </summary>
         Epsilon,
     }
 }
